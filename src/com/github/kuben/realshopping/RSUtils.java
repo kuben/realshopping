38813b49bb4205c4fc16063bff8a71171582a0f5
stengun <stengun@garlach>
2014-05-20 02:50:45 +0200

    Last update license

--- src/com/github/kuben/realshopping/RSUtils.java
+++ src/com/github/kuben/realshopping/RSUtils.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2013-2014 Jakub Fojt, Roberto Benfatto
+ * This file is part of RealShopping.
+ * 
+ * RealShopping is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * RealShopping is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with RealShopping.  If not, see <http://www.gnu.org/licenses/>.
+ *     
+ */
+
 package com.github.kuben.realshopping;
 
 import static com.github.kuben.realshopping.Shop.sellPrice;
--- src/com/github/stengun/realshopping/InventoryManager.java
+++ src/com/github/stengun/realshopping/InventoryManager.java
@@ -1,19 +1,19 @@
 /*
- * RealShopping Bukkit plugin for Minecraft
- * Copyright 2013 Jakub Fojt
+ * Copyright (C) 2013-2014 Jakub Fojt, Roberto Benfatto
+ * This file is part of RealShopping.
  * 
- * This program is free software: you can redistribute it and/or modify
+ * RealShopping is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  * 
- * This program is distributed in the hope that it will be useful,
+ * RealShopping is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  * 
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * along with RealShopping.  If not, see <http://www.gnu.org/licenses/>.
  *     
  */
 
